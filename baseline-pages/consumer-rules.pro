3ef578800c6f6c63e0dc1e7a888bf484cc17c780
caixiao <cai.xiao@hotmail.com>
2026-04-27 16:19:29 +0800

    新增基准页面模块

---
+++ baseline-pages/consumer-rules.pro
@@ -0,0 +1,1 @@
+# Consumer ProGuard rules for baseline-pages.
---
+++ baseline-pages/src/main/java/com/hh/uiperception/baseline/BaselinePages.java
@@ -0,0 +1,8 @@
+package com.hh.uiperception.baseline;
+
+public final class BaselinePages {
+    public static final String MODULE_NAME = "baseline-pages";
+
+    private BaselinePages() {
+    }
+}
